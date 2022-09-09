91cc9f481c7bb9cca49bc7372b99ec982e03e34e
watermelononsunday <ndoyaindumbi@gmail.com>
2022-09-09 08:53:51 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 100 ; alphabet >= 99; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}

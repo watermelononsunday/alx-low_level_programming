363e24fbb058d18c7bed6bdb33ec9fc55678cc10
watermelononsunday <ndoyaindumbi@gmail.com>
2022-09-14 08:10:42 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+
+{
+char alphabet;
+char number;
+
+for (number = 0; number <=9; number++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
+}

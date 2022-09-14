02fdd6586274f346d98d8171fe3f2c267ce5e03d
watermelononsunday <ndoyaindumbi@gmail.com>
2022-09-14 11:26:38 -0700

    digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ if (n < 0)
 {
 last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
-return (last _digit);
+return (last_digit);
 }
 
 else
 {
-last _digit = (n %10);
+last _digit = (n % 10);
 _putchar (last_digit +'0');
 return (last_digit);
 }

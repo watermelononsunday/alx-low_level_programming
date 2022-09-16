528cb3beb7523c37a1e9c2d934b18636ce2278c6
watermelononsunday <ndoyaindumbi@gmail.com>
2022-09-16 09:22:05 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int a;
+
+a = 0;
+while (a < 10)
+{
+_putchar(a + '0');
+a++;
+}
+_putchar('\n');
+}

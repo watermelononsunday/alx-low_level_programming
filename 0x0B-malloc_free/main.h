8b4ac83cc19ce8a463fe10f97d61b53ccaf6016c
watermelononsunday <ndoyaindumbi@gmail.com>
2022-10-04 02:42:17 -0700

    a function that returns a pointer to a newly allocated sp
    ace in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 int _putchar(char c);
 void free_grid(int **grid, int height);
-char *create_array(unsinged int size, char c);
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av);

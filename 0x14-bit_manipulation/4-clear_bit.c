f3c4353bbe79071cee886a846d15d6281fca1c94
watermelononsunday <ndoyaindumbi@gmail.com>
2022-11-03 04:18:49 -0700

    a function that sets the value of a bit to 0 at a gi
    ven index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * clear_bit - set the value of a bit to 0 at a given index
- * @n: pointer to decimal position to change
+ * @n: pointer to decimal number to change
  * @index: index position to change
  * Return: 1 if it worked, -1 if error
  */
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int i;
 	unsigned int hold;
 
-	if 9index > 64)
+	if (index > 64)
 		return (-1);
 	hold = index;
 	for (i = 1; hold > 0; i *= 2, hold--)
